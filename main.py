2ef2d72ba7b926716e01e4e9941f8ba2947055a9
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2025-06-17 12:44:59 +0545

    video_works_frames_received

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+
+# time.sleep(2)
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-else:
-    print("camera working!")
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Cannot receive frame")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(0) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
